037131106abeb4b0565a058843f4c9187040c25a
3lackrush <505478266@qq.com>
2017-07-14 17:42:24 +0800

    添加获取Token方法

--- Kios/zoomeye/views.py
+++ Kios/zoomeye/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
+import json
+from APIconfig.config import *
 # Create your views here.
+
+def index(request):
+	print(USERNAME,PASSWORD)
+	payload = json.dumps({"username":USERNAME,"password":PASSWORD})
+	print(payload)
+	r = requests.post("https://api.zoomeye.org/user/login", data=payload)
+	return HttpResponse(r.content)
